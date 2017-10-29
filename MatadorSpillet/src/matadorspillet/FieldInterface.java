8a5d341082572d6dc1d6e09a34b57ceff2a6f7c6
Snowball44 <32239917+Snowball44@users.noreply.github.com>
2017-10-29 21:58:12 +0100

    Uploaded part 2 of assignment

---
+++ MatadorSpillet/src/matadorspillet/FieldInterface.java
@@ -0,0 +1,15 @@
+
+package matadorspillet;
+
+/**
+ *
+ * @author erso
+ */
+public interface FieldInterface
+{
+    // Disse metoder er underforstået 'public abstract static': 
+    String getName();
+    int getNumber();
+    void consequense(Player poorPlayer);
+    
+}
---
+++ MatadorSpillet/src/matadorspillet/GoToJailField.java
@@ -0,0 +1,46 @@
+package matadorspillet;
+
+/**
+ *
+ * @author erso
+ */
+public class GoToJailField implements FieldInterface
+{
+
+    private String name;
+    private int number;
+
+    public GoToJailField(String name, int number)
+    {
+        this.name = name;
+        this.number = number;
+    }
+
+    @Override
+    public String getName()
+    {
+        return name;
+    }
+
+    @Override
+    public int getNumber()
+    {
+        return number;
+    }
+
+    @Override
+    public void consequense(Player poorPlayer)
+    {
+        poorPlayer.setPos(MonopolyConstants.JAIL_POS);
+        
+    }
+
+    @Override
+    public String toString()
+    {
+        return "GoToJailField{" + "name=" + name + ", number=" + number + '}';
+    }
+    
+    
+
+}
